cbe0c0cc86a57440556bc17ac1bfafc5382e13d4
Durkh <egidio15954@gmail.com>
2025-03-29 23:20:27 -0300

    first implementation rpico2 using DCP [UT]

--- Pico2/FreeRTOSConfig_examples_common.h
+++ Pico2/FreeRTOSConfig_examples_common.h
@@ -61,6 +61,8 @@
 #define configUSE_QUEUE_SETS                    1
 #define configUSE_TIME_SLICING                  1
 #define configUSE_NEWLIB_REENTRANT              0
+#define configUSE_TASK_NOTIFICATIONS            1
+#define configTASK_NOTIFICATION_ARRAY_ENTRIES   1
 // todo need this for lwip FreeRTOS sys_arch to compile
 #define configENABLE_BACKWARD_COMPATIBILITY     1
 #define configNUM_THREAD_LOCAL_STORAGE_POINTERS 5
