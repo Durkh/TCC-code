19466d35b9212c2efee7f876c71eb452ef9f6a8f
Egídio Neto <egidioneto.alves@gmail.com>
2025-03-31 17:26:35 -0300

    all modules working, starting integration

--- ESP32C3/main/main.cpp
+++ ESP32C3/main/main.cpp
@@ -31,14 +31,8 @@ extern "C" void app_main(void){
     initArduino();
     Serial.begin(115200);
 
-    //unsigned char msg[] = "egidio neto da computacao";
-
     DCP_MODE mode;
-#ifdef READ
-    mode.addr = 1;
-#else
     mode.addr = 2;
-#endif
     mode.flags.flags = FLAG_Instant;
     mode.isController = true;
     //mode.speed = DCP_MODE::FAST2;
@@ -48,23 +42,6 @@ extern "C" void app_main(void){
         ESP_LOGE("MAIN", "Error initializing bus");
         return;
     }
-
-#ifdef READ
-    struct DCP_Message_t* message = NULL;
-
-    while(1){
-        message = ReadMessage();
-
-        if (message) {
-            DCP_Data_t debug = {.message = message};
-            SanityCheck(message->type, debug.data);
-            free(message);
-            message = NULL;
-        }
-
-        vTaskDelay(pdMS_TO_TICKS(50));
-    }
-#else 
 
     ens160.setI2C(4, 3);
     Wire.setPins(4, 3);
@@ -88,40 +65,40 @@ extern "C" void app_main(void){
     }
 
     sensors_event_t humidity1, temp;
-
-
-    unsigned char msg[] = "egidio";
+    struct {float t; float h; float v; float a; float c;} sensorData;
 
     while(1){
 
         if (aht.getStatus() != 0xFF) {
             aht.getEvent(&humidity1, &temp);// populate temp and humidity objects with fresh data
 
-            ESP_LOGI("AHT", "temp: %.2f\thumidity: %.2f", temp.temperature, humidity1.relative_humidity);
+            ESP_LOGD("AHT", "temp: %.2f\thumidity: %.2f", temp.temperature, humidity1.relative_humidity);
+            sensorData.t = temp.temperature;
+            sensorData.h = humidity1.relative_humidity;
 
             if (ens160.available()) {
                 ens160.set_envdata(temp.temperature, humidity1.relative_humidity);
-
                 ens160.measure(true);
                 ens160.measureRaw(true);
 
-                ESP_LOGI("ENS", "AQI: %i\tTVOC: %ippb\teCO2: %ippm\t", ens160.getAQI(), ens160.getTVOC(), ens160.geteCO2());
+                sensorData.a = ens160.getAQI();
+                sensorData.v = ens160.getTVOC();
+                sensorData.c = ens160.geteCO2();
+
+                ESP_LOGD("ENS", "AQI: %i\tTVOC: %ippb\teCO2: %ippm\t", ens160.getAQI(), ens160.getTVOC(), ens160.geteCO2());
             }
         }
 
+        DCP_Data_t message = {.data = (uint8_t*)malloc(sizeof(struct DCP_Message_Generic_t) + sizeof sensorData + sizeof(uint8_t))};
 
-        DCP_Data_t message = {.data = (uint8_t*)malloc(sizeof(struct DCP_Message_Generic_t) + strlen((char*)msg)+1 + sizeof(uint8_t))};
-
-        message.message->type = strlen((char*)msg)+3;
+        message.message->type = sizeof sensorData + 2;
         message.message->generic.addr = mode.addr;
-        memcpy(message.message->generic.payload, msg, strlen((char*)msg)+1);
+
+        memcpy(message.message->generic.payload, &sensorData, sizeof sensorData);
         SanityCheck(message.message->type, message.data);
 
         SendMessage(message);
-
-        vTaskDelay(pdMS_TO_TICKS(100));
     }
-#endif
 
     while(true);
 
