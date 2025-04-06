d25dd40fb1f2fe9dec0c41bf19e7a0d58ddc7739
Egídio Neto <egidioneto.alves@gmail.com>
2025-04-06 19:19:44 -0300

    fix: pico sending the button message

--- Pico2/main/main.c
+++ Pico2/main/main.c
@@ -20,7 +20,7 @@ static struct DCP_Message_t buttonData = {
         .IDS = DCP_ADDR,
         .IDD = 0xF0,
         .COD = 0x1,
-        .data = {0x0},
+        .data = {0xFF},
         .PAD = 0x0
     }
 };
@@ -36,14 +36,12 @@ void main_task(void *params) {
         if (message) {
 
 	    memcpy(&sensorData, &message->generic.payload, sizeof sensorData);
-	    /*
 	    printf("t:%.2f\th:%.2f\tv:%i\ta:%i\tc:%i\n",
 			    sensorData.t,
 			    sensorData.h,
 			    sensorData.v,
 			    sensorData.a,
 			    sensorData.c);
-	    */
 
 	    gpio_put(14, sensorData.t > 35);
 	    gpio_put(15, sensorData.c > 650);
@@ -52,15 +50,15 @@ void main_task(void *params) {
             message = NULL;
         }
 
-	vTaskDelay(pdMS_TO_TICKS(10));
+        vTaskDelay(pdMS_TO_TICKS(5));
 
-	if(buttonData.L3.data[0] != 0xFF){
+        if(buttonData.L3.data[0] != 0xFF){
             DCP_Data_t buttonMessage = (DCP_Data_t){.data = malloc(sizeof (struct DCP_Message_t))};
             memcpy((void*)buttonMessage.message, &buttonData, sizeof buttonData);
 
-	    SendMessage(buttonMessage);
-	    buttonData.L3.data[0] = 0xFF;
-	}
+            SendMessage(buttonMessage);
+            buttonData.L3.data[0] = 0xFF;
+        }
 
     }
 }
@@ -71,13 +69,15 @@ void ButtonCallback(void){
     if (event & GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL){
         gpio_acknowledge_irq(10, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL);
 
+        if(buttonData.L3.data[0] != 0xFF) return;
+
         if(event == GPIO_IRQ_EDGE_RISE){
-		gpio_put(13,1);
-	    buttonData.L3.data[0] = 0x1;
-	}else if (event == GPIO_IRQ_EDGE_FALL){
-		gpio_put(13,0);
-	    buttonData.L3.data[0] = 0x0;
-	}
+            gpio_put(13,1);
+            buttonData.L3.data[0] = 0x1;
+        }else if (event == GPIO_IRQ_EDGE_FALL){
+            gpio_put(13,0);
+            buttonData.L3.data[0] = 0x0;
+        }
     }
 }
 
@@ -86,16 +86,16 @@ int main( void )
     stdio_init_all();
 
     //LEDs 
-    gpio_init_mask(1U<<15 | 1U<<14 | 1U<<13 | 1U<<12 | 1U<<11);
-    gpio_set_dir_out_masked(1U<<15 | 1U<<14 | 1U<<13 | 1U<<12 | 1U<<11);
+    gpio_init_mask(1U<<15 | 1U<<14 | 1U<<13 | 1U<<12 | 1U<<11 | 1U<<28);
+    gpio_set_dir_out_masked(1U<<15 | 1U<<14 | 1U<<13 | 1U<<12 | 1U<<11 | 1U<<28);
 
     //button
     gpio_init(10);
-    //gpio_set_dir(10, GPIO_IN);
-    //gpio_set_irq_enabled(10, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true);
+    gpio_set_dir(10, GPIO_IN);
+    gpio_set_irq_enabled(10, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true);
     gpio_add_raw_irq_handler(10, &ButtonCallback);
 
-    DCP_MODE mode = {.addr = DCP_ADDR, .flags.flags = FLAG_Instant, .isController = 0, .speed = SLOW};
+    DCP_MODE mode = {.addr = DCP_ADDR, .flags.flags = FLAG_Instant, .isController = 1, .speed = SLOW};
 
     if (!DCPInit(2, mode)){
         printf("não foi possível iniciar o barramento\n");
